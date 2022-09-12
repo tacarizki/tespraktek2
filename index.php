894c7eb651d9061b3647d84480b3fe2d57abfd44
tacarizki <109931207+tacarizki@users.noreply.github.com>
2022-09-12 10:38:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,121 @@
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>JUAL MOBIL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1></h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+<body>
+<header >
+    <h1 class="bg-primary text-white"> TACA RIZKI MANJAYA </h1>
+    <nav>
+    <ul class="nav bg-secondary ">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+      <li class="nav-item"> <a class="nav-link" href="#"> Home </a> </li>
+      <li class="nav-item"> <a class="nav-link" href="profil.php"> Profil </a> </li>
+      <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              merek
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">bmw</a></li>
+              <li><a class="dropdown-item" href="#">jeep</a></li>
+              <li><a class="dropdown-item" href="#">nisan</a></li>
+              <li><a class="dropdown-item" href="#">mustang</a></li>
+              <li><a class="dropdown-item" href="#"></a></li>
+              <li><hr class="dropdown-divider"> </li>
+              <li><a class="dropdown-item" href="login.php">login</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              bagian
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">nitro</a></li>
+              <li><a class="dropdown-item" href="#">ban</a></li>
+              <li><a class="dropdown-item" href="#">serep</a></li>
+              <li><a class="dropdown-item" href="#">knalpot</a></li>
+              <li><a class="dropdown-item" href="#"></a></li>
+              <li><hr class="dropdown-divider"> </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+    </ul>
+  </nav>
+ </header>
+ <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="gambar1.jpeg" class="d-block w-100" alt="gambar1">
+      </div>
+      <div class="carousel-item">
+        <img src="gambar2.jpg" class="d-block w-100" alt="gambar2">
+      </div>
+      <div class="carousel-item">
+        <img src="gambar3.jpg" class="d-block w-100" alt="gambar3">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+ <section>
+ <div class="card-group">
+  <div class="card">
+    <img src="gambarwa.webp" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">WHATSAPP</h5>
+      <p class="card-text">Aplikasi WhatsApp Web dan WhatsApp Desktop adalah ekstensi berbasis komputer dari akun WhatsApp pada telepon Anda.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="gambarig.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">INSTAGRAM</h5>
+      <p class="card-text">Instagram adalah sosial media berbasis gambar yang memberikan layanan berbagi foto atau video secara online.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="gambarfb.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">FACEBOOK</h5>
+      <p class="card-text">Facebook adalah media sosial dan layanan jejaring sosial online Amerika yang dimiliki oleh Meta Platforms.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+ </section>
+ <footer>
+ <div class="card">
+    <div class="card-header">
+      Exit
+    </div>
+    <div class="card-body">
+      <blockquote class="blockquote mb-0">
+        <p>Taca Rizki Manjaya</p>
+        <footer class="blockquote-footer">Good Bye <cite title="Source Title"></cite></footer>
+      </blockquote>
+    </div>
+  </div>
+ </footer>
+</body>
+</html>
---
+++ profil.php
@@ -0,0 +1,26 @@
+<div class="card-group">
+  <div class="card">
+    <img src="gambarms.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">MASSANGER</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="gambartt.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">TIKTOK</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="gambaryt.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">YOUTUBE</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
